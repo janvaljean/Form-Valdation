4b5b3d72ab0c75e6de61621997c93653937f8817
janvaljean <kurtuldu@gmx.de>
2023-03-10 02:56:20 +0100

    error messages added

--- my-app/src/components/Validation.jsx
+++ my-app/src/components/Validation.jsx
@@ -2,9 +2,9 @@ import * as yup from 'yup';
 
 const validations = yup.object().shape({
  
-  email: yup.string().email().required(),
+  email: yup.string().email("gecerli bir email gir ulan").required(),
   password: yup.string().min(5).required(),
-  passwordConfirm: yup.string().oneOf([yup.ref('password'), null]).required()
+  passwordConfirm: yup.string().oneOf([yup.ref('password'), null],"parola uyusmuyor").required()
 
 
   
