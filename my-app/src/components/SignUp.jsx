d1fd52aedd974a26ee050ba3ee8dbe7d17f8be3c
janvaljean <kurtuldu@gmx.de>
2023-03-10 02:51:03 +0100

    error && touched && handleblur added

--- my-app/src/components/SignUp.jsx
+++ my-app/src/components/SignUp.jsx
@@ -3,7 +3,7 @@ import { useFormik } from 'formik';
 import validations from './Validation';
 
 const SignUp = () => {
-    const {handleChange,handleSubmit,values} = useFormik({
+    const {handleChange,handleSubmit,handleBlur,values,errors,touched} = useFormik({
      initialValues: {
         passwordConfirm: '',
         password: '',
@@ -30,21 +30,27 @@ const SignUp = () => {
           placeholder="jane@acme.com"
           type="email"
           value={values.email}
+          onBlur = {handleBlur}
         />
+        {errors.email && touched.email&& <div className='text-danger'>{errors.email}</div>}  
         <label htmlFor="password">password</label>
         <input onChange={handleChange}
           name="password"
           placeholder="********"	
           type="password"
           value={values.password}
+          onBlur = {handleBlur}
         />
+        {errors.password && touched.password && <div className='text-danger'>{errors.password}</div>}  
         <label htmlFor="passwordConfirm">passwordConfirm</label>
         <input onChange={handleChange}
+          onBlur = {handleBlur}
           name="passwordConfirm"
           placeholder="********"	
           type="password"
           value={values.passwordConfirm}
         />
+        {errors.passwordConfirm && touched.passwordConfirm && <div className='text-danger'>{errors.passwordConfirm}</div>}  
    
         <button type="submit">Submit</button>
        
